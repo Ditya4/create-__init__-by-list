09efe86b971195233105720534149ffa9377ce9a
Ditya4 <72632156+Ditya4@users.noreply.github.com>
2020-10-26 22:24:43 +0200

    writing a read file function

--- from_list_to_init.py
+++ from_list_to_init.py
@@ -1,5 +1,5 @@
 def print_parameters(start_text, list_to_transform):
-    print(start_text, end='')
+    print(start_text, "index=None, ", end='', sep='')
     index = 0
     while index < len(list_to_transform):
         out_index = index
@@ -20,20 +20,75 @@ def print_parameters(start_text, list_to_transform):
             print('\n' + ' ' * (len(start_text) - 6), end='')
         else:
             print()
-    #print()
-        
-        
+
+
 def values_assigment(start_text, list_to_transform):
     start_spases = len(start_text) - 15
+    print(' '*start_spases, 'self.index = index', sep='')
     for index in range(len(list_to_transform)):
         print(' '*start_spases, end='')
         print('self.', list_to_transform[index], ' = ',
               list_to_transform[index], sep='')
-        
-        # print()
+
+
+def create_init(start_text, list_to_transform):
+    print_parameters(start_text, list_to_transform)
+    values_assigment(start_text, list_to_transform)
+
+
+
+
+
+
+def create_file_read_function(file_place, name_of_records,
+                              len_of_elements, class_name):
+    print(f'def read_{name_of_records}({name_of_records}_file_name):')
+    print(f'    {name_of_records}_file = open({name_of_records}_file_name)',
+    f'    {name_of_records}_lines = {name_of_records}_file.readlines()',
+    f'    size_of_{name_of_records}_list = len({name_of_records}_lines)',
+    f'    for index in range(size_of_{name_of_records}_list):',
+    f'        {name_of_records}_lines[index] = (',
+    f'               {name_of_records}_lines[index].rstrip()',
+    f'    {name_of_records} = [None] * size_of_{name_of_records}',
+    f'    in_{name_of_records}_list_index = 0',
+    f'    out_{name_of_records}_list_index = 0',
+    f'    while in_{name_of_records}_list_index < size_of_{name_of_records}_list:',
+    f'        line_split =',
+    f'               {name_of_records}[in_{name_of_records}_list_index].split("\\t")',
+    f'        if line_split[-1] == "\\n":',
+    f'            line_split.pop()',
+    f'        print(in_{name_of_records}_list_index, "line_split =", line_split)',
+    f'        if len(line_split) == {len_of_elements}:',
+    f'            {name_of_records}[out_{name_of_records}_list_index] = (',
+    f'                            {class_name}(out_{name_of_records}_list_index,',
+    f'                            *line_split)',
+    f'            in_{name_of_records}_list_index += 1',
+    f'            out_{name_of_records}_list_index += 1',
+    f'        else:',
+    '            print(f"Error in line from file = {file_place}",',
+    '                  f"with index = {in_%s_list_index}",'%name_of_records,
+    '                  f"with value {line_split}",',
+    '                  f"wait for {len_of_elements} parameters",',
+    '                  f"and got len(line_split)")',
     
+    sep='\n')
     
 
+
+
+
+
+
+
+
+
+
+
+'''
+### TODO
+we need to create a class with name camel_style
+cause we use it in read function
+'''
 string_to_transform ='''
         switch_id,switch_name,rs_id,rs_name,
         cday,h0,h1,h2,h3,
@@ -41,14 +96,22 @@ string_to_transform ='''
         h9,h10,h11,h12,h13,
         h14,h15,h16,h17,h18,h19,H20,h21,h22,h23
 '''
-list_to_transform = string_to_transform.split(',')
-for index in range(len(list_to_transform)):
-    list_to_transform[index] = list_to_transform[index].strip().rstrip().lower()
-print(list_to_transform)
-
 start_text = "    def __init__(self, "
-print_parameters(start_text, list_to_transform)
-values_assigment(start_text, list_to_transform)
+file_place = 'D:\\python\\double_dno\\by_hours\\mzts-3\\mzts_3_ama_all_time.txt'
+name_of_records = 'by_hours_records'
+class_name = ''.join(word.title() for word in name_of_records.split('_'))
+print(f'class {class_name}:')
+list_to_transform = string_to_transform.split(',')
+len_of_elements = len(list_to_transform)
+for index in range(len_of_elements):
+    list_to_transform[index] = list_to_transform[index].strip().rstrip().lower()
+
+# print(list_to_transform)
+
+create_init(start_text, list_to_transform)
+
+create_file_read_function(file_place, name_of_records, len_of_elements, class_name)
+
 
 
 
